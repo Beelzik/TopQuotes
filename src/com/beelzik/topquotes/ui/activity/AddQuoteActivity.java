4ac5223455dc973b257d0ee6c67dfce14cfcf63e
Beelzik <beelzik@gmail.com>
2014-07-02 15:29:42 +0300

    -

--- src/com/beelzik/topquotes/ui/activity/AddQuoteActivity.java
+++ src/com/beelzik/topquotes/ui/activity/AddQuoteActivity.java
@@ -10,6 +10,7 @@ import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
+
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
